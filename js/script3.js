99f8669c31cdbcb2a7d2e96f333e1adbcdf50070
Henrique Bertellini de Ávila <henriquebertellini@gmail.com>
2023-06-30 15:06:39 -0300

    up

--- js/script3.js
+++ js/script3.js
@@ -1,6 +1,11 @@
 let num = prompt('Digite o seu número')
+
 let cont = 1
 while((cont-1) != num){
+    if(num < 0 || num != Number()){
+        alert("Inválido")
+        break;
+    }
     console.log(cont)
     cont++
 }
